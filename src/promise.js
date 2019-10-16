e6908d05456760cbe6b42adf10896c5514958d0b
Hector DelaRosa <hdelaros@gmail.com>
2019-10-15 23:54:37 -0400

    finished all test specs except chainedPromise

--- src/promise.js
+++ src/promise.js
@@ -1,10 +1,22 @@
-const basicPromise = new Promise( (res) => {
-    res('basicPromise');
-})
-const funcPromise = () => new Promise( (res) => {
-    res('funcPromise');
+const basicPromise = new Promise(res => {
+  res('basicPromise');
 });
-const chainedPromise = prom => null;
-const rejectedPromise = () => null;
+const funcPromise = () =>
+  new Promise(res => {
+    res('funcPromise');
+  });
+const chainedPromise = prom => {
+  let promise = new Promise(res => {
+    res(prom);
+  }).then( val => val );
+  return promise;
+};
+const rejectedPromise = () => {
+    const promise = new Promise ( (res,rej) => {
+        const reason = new Error('rejectedPromise'); 
+        rej(reason);
+    })
+    return promise;
+};
 
 module.exports = { basicPromise, chainedPromise, rejectedPromise, funcPromise };
